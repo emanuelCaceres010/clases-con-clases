79fd1837c23980f7f58cbdcf4be760e817619b0d
Emanuel Caceres <emanuelcaceres010@gmail.com>
2022-09-23 15:33:52 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,42 @@
+class Banda {
+    members: string[];
+    albums: object[];
+    constructor(members: string[], albums: object[]){
+        this.members = members;
+        this.albums = albums;
+    };
+};
+//le indicamos al constructor que albums tiene que recibir el tipo de dato object[], o sea, cualquier array de objetos. Esa es la única condición.
+
+//Ahora, imaginemos que queremos ser más específicxs. En lugar de indicarle a albums que tiene que recibir un object[], podemos decirle que espere recibir otra clase, a la que le vamos a definir ciertas características.
+
+//Veamos el ejemplo:
+
+class Album {
+    title: string;
+    songs: string[];
+    constructor(title, songs:string[]){
+        this.title = title;
+        this.songs = songs;
+    };
+};
+
+
+
+class Banda2 {
+    members: string[];
+    albums: object[];
+    constructor(members: string[], albums: Album[]){
+        this.members = members;
+        this.albums = albums;
+    };
+};
+
+//function main(){
+    const unAlbum= new Album ("album 1",[]);
+
+    const unaBanda= new Banda2(["ema"], [unAlbum, unAlbum, unAlbum]);
+    console.log(unaBanda.albums);
+//}
+
+//main();
